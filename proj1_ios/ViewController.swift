a58a87d14b9ade5d46d5b69e9aba28da25d3d036
m98-mac <m.annayev98@gmail.com>
2024-05-22 23:51:14 -0700

    Initial Commit

---
+++ proj1_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj1_ios
+//
+//  Created by m98 on 22.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
